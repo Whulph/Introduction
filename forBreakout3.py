ae2eae7fe703d842cacd283df2d7d5ec3f9d2b06
Tyler Wolf <tylerwolf2290@gmail.com>
2021-06-16 15:26:16 -0400

    Added Exercises

--- forBreakout3.py
+++ forBreakout3.py
@@ -4,18 +4,19 @@ Created on Mon Jun 14 15:45:48 2021
 
 @author: Tyler
 """
-
+actualPassword = 'password'
 
 attempts = 1
 
-while attempts <= 3:
+while True:
     x = input("Enter your password:")
     
-    if x == "password":
+    if x == actualPassword:
         print("Welcome in")
         break
     elif attempts==3:
         print("You are not allowed access to this computer!")
+        break
     else:
         print("Wrong password. Please try again.")
         
